85c7ff7b420f25214b4c20abd4855a7c9070388b
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2024-05-04 19:55:32 -0300

    fix: restaurant favorite button

--- app/_components/restaurant-item.tsx
+++ app/_components/restaurant-item.tsx
@@ -26,7 +26,7 @@ const RestaurantItem = ({ restaurant }: RestaurantItemProps) => {
 
         <Button
           size="icon"
-          className="absolute right-2 top-2 h-7 w-7 rounded-full bg-gray-700"
+          className="absolute right-2 top-2 h-7 w-7 rounded-full bg-gray-700/50"
         >
           <HeartIcon size={16} className="fill-white" />
         </Button>
