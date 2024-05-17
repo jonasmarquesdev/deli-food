49c90216a2d7137a78ffc8c411dbf554f8e8ab39
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2024-05-16 21:51:51 -0300

    feat: add cart and cart item components

---
+++ app/_context/cart.tsx
@@ -0,0 +1,163 @@
+/* eslint-disable no-unused-vars */
+"use client";
+
+import { Prisma, Product } from "@prisma/client";
+import { createContext, ReactNode, useMemo, useState } from "react";
+import { calculateProductTotalPrice } from "../_helpers/price";
+
+export interface CartProduct
+  extends Prisma.ProductGetPayload<{
+    include: {
+      restaurant: {
+        select: {
+          deliveryFee: true;
+        };
+      };
+    };
+  }> {
+  quantity: number;
+}
+
+interface ICartContext {
+  products: CartProduct[];
+  subTotalPrice: number;
+  totalPrice: number;
+  totalDiscounts: number;
+  addProductsToCart: (
+    product: Prisma.ProductGetPayload<{
+      include: {
+        restaurant: {
+          select: {
+            deliveryFee: true;
+          };
+        };
+      };
+    }>,
+    quantity: number,
+  ) => void;
+  decreaseProductQuantity: (productId: string) => void;
+  increaseProductQuantity: (productId: string) => void;
+  removeProductFromCart: (productId: string) => void;
+}
+
+export const CartContext = createContext<ICartContext>({
+  products: [],
+  subTotalPrice: 0,
+  totalPrice: 0,
+  totalDiscounts: 0,
+  addProductsToCart: () => {},
+  decreaseProductQuantity: () => {},
+  increaseProductQuantity: () => {},
+  removeProductFromCart: () => {},
+});
+
+export const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [products, setProducts] = useState<CartProduct[]>([]);
+
+  const subTotalPrice = useMemo(() => {
+    return products.reduce((acc, product) => {
+      return acc + Number(product.price) * product.quantity;
+    }, 0);
+  }, [products]);
+
+  const totalPrice = useMemo(() => {
+    return products.reduce((acc, product) => {
+      return acc + calculateProductTotalPrice(product) * product.quantity;
+    }, 0);
+  }, [products]);
+
+  const totalDiscounts = subTotalPrice - totalPrice;
+
+  const decreaseProductQuantity = (productId: string) => {
+    return setProducts((prev) =>
+      prev.map((cartProduct) => {
+        if (cartProduct.id === productId) {
+          if (cartProduct.quantity === 1) {
+            return cartProduct;
+          }
+
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity - 1,
+          };
+        }
+
+        return cartProduct;
+      }),
+    );
+  };
+
+  const increaseProductQuantity = (productId: string) => {
+    return setProducts((prev) =>
+      prev.map((cartProduct) => {
+        if (cartProduct.id === productId) {
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + 1,
+          };
+        }
+
+        return cartProduct;
+      }),
+    );
+  };
+
+  const removeProductFromCart = (productId: string) => {
+    return setProducts((prev) =>
+      prev.filter((product) => product.id !== productId),
+    );
+  };
+
+  const addProductsToCart = (
+    product: Prisma.ProductGetPayload<{
+      include: {
+        restaurant: {
+          select: {
+            deliveryFee: true;
+          };
+        };
+      };
+    }>,
+    quantity: number,
+  ) => {
+    // VERIFICAR SE O PRODUTO JÁ ESTA NO CARRINHO
+    const isProductAlreadyOnCart = products.some(
+      (cartProduct) => cartProduct.id === product.id,
+    );
+    // SE ELE ESTIVER, AUMENTAR A SUA QUANTIDADE
+    if (isProductAlreadyOnCart) {
+      return setProducts((prev) =>
+        prev.map((cartProduct) => {
+          if (cartProduct.id === product.id) {
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity + quantity,
+            };
+          }
+
+          return cartProduct;
+        }),
+      );
+    }
+
+    // SE NÃO, ADICIONÁ-LO COM A QUANTIDADE RECEBIDA
+    setProducts((prev) => [...prev, { ...product, quantity: quantity }]);
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        products,
+        subTotalPrice,
+        totalPrice,
+        totalDiscounts,
+        addProductsToCart,
+        decreaseProductQuantity,
+        increaseProductQuantity,
+        removeProductFromCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
