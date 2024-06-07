515be931230eef3d819a1b689872141a6ebbdf9d
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2024-06-07 06:42:18 -0300

    feat: add favorite restaurants page

--- app/_components/restaurant-item.tsx
+++ app/_components/restaurant-item.tsx
@@ -9,28 +9,29 @@ import Link from "next/link";
 import { cn } from "../_lib/utils";
 import { toggleFavoriteRestaurant } from "../_actions/restaurant";
 import { toast } from "sonner";
+import { useSession } from "next-auth/react";
 
 interface RestaurantItemProps {
-  userId?: string;
   restaurant: Restaurant;
   className?: string;
-  userFavoritesRestaurants?: UserFavoriteRestaurant[];
+  userFavoritesRestaurants: UserFavoriteRestaurant[];
 }
 
 const RestaurantItem = ({
   restaurant,
   className,
-  userId,
   userFavoritesRestaurants,
 }: RestaurantItemProps) => {
+  const { data } = useSession();
+
   const isFavorite = userFavoritesRestaurants?.some(
     (fav) => fav.restaurantId === restaurant.id,
   );
 
   const handleFavoriteClick = async () => {
-    if (!userId) return;
+    if (!data?.user.id) return;
     try {
-      await toggleFavoriteRestaurant(userId, restaurant.id);
+      await toggleFavoriteRestaurant(data?.user.id, restaurant.id);
       toast.success(
         isFavorite
           ? "Restaurante removido dos favoritos."
@@ -59,7 +60,7 @@ const RestaurantItem = ({
             <span className="text-xs font-semibold">5.0</span>
           </div>
 
-          {userId && (
+          {data?.user.id && (
             <Button
               size="icon"
               className={`absolute right-2 top-2 h-7 w-7 rounded-full bg-gray-700/50 ${isFavorite && "bg-primary hover:bg-gray-700"}`}
